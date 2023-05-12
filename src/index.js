aa7fae05ae6ecefd06e196f62aaa0b50dd02357c
Taras <117749635+vasylkivt@users.noreply.github.com>
2023-05-12 15:05:31 +0300

    add_loader

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import { makeGalleryMarkup } from './js/makeGalleryMarkup';
 import { fetchImages } from './js/fetchImages';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
 import SimpleLightbox from 'simplelightbox';
 
 import 'modern-normalize/modern-normalize.css';
-// import 'pretty-checkbox/src/pretty-checkbox.scss';
 import 'pretty-checkbox/dist/pretty-checkbox.min.css';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
@@ -13,8 +13,9 @@ const gallerySLBox = new SimpleLightbox('.gallery-list a');
 const formEl = document.getElementById('search-form');
 const galleryEl = document.getElementById('gallery-list');
 const loadMoreEl = document.querySelector('.load-more');
-const scrollGuardEl = document.querySelector('.scroll-guard');
+const objectForObservation = document.querySelector('.object-for-observation');
 const infiniteScrollEl = document.querySelector('.infinite-scroll-input');
+const backdropLoaderEl = document.querySelector('.backdrop-loader');
 
 const IMAGE_PER_PAGE = 40;
 let searchName;
@@ -27,20 +28,38 @@ formEl.addEventListener('submit', onSubmit);
 
 async function onSubmit(e) {
   e.preventDefault();
-
   galleryEl.innerHTML = '';
   page = 1;
   searchName = e.currentTarget.elements.searchQuery.value;
 
+  if (!searchName.trim()) {
+    await new Promise(resolve => {
+      Confirm.show(
+        '',
+        'Ви хочите здійснити пошук з порожнім полем?',
+        'Так',
+        'Ні',
+        () => {
+          resolve(); // Викликаємо resolve, щоб продовжити виконання коду
+        },
+        () => {
+          resetToDefault();
+          return;
+        },
+        {
+          width: '350px',
+        }
+      );
+    });
+  }
+
+  await beforeSearch();
   try {
     const { data } = await fetchImages(searchName, IMAGE_PER_PAGE, page);
-    if (data.totalHits > IMAGE_PER_PAGE) {
-      loadMoreEl.classList.add('load-more-show');
-      addButtonLoadMore();
-    }
 
     if (data.totalHits) {
       Notify.success(`Ура! Ми знайшли ${data.total} картинок.`);
+      totalPage = data.totalHits / IMAGE_PER_PAGE;
     }
 
     if (data.totalHits === data.total && data.total) {
@@ -49,78 +68,59 @@ async function onSubmit(e) {
       Notify.info(`Вам доступно ${data.totalHits} результатів пошуку.`);
     }
 
+    await objectForObservation.classList.add('object-for-observation-show');
+
     await onFetchSuccess(data);
-    await gallerySLBox.refresh();
-
-    totalPage = await (data.totalHits / IMAGE_PER_PAGE);
-
-    await scrollGuardEl.classList.add('scroll-guard-show');
   } catch (error) {
-    onFetchError(error);
+    Notify.failure(error.message);
+    resetToDefault();
   }
 }
 
-function onFetchSuccess({ hits }) {
+function beforeSearch() {
+  backdropLoaderEl.classList.add('backdrop-loader-show');
+  document.body.style.overflow = 'hidden';
+}
+
+async function onFetchSuccess({ hits }) {
   if (hits.length === 0) {
     Notify.failure(
       'На жаль, немає зображень, що відповідають вашому запиту. Будь ласка, спробуйте ще раз.'
     );
+    await resetToDefault();
+
     return;
   }
-  galleryEl.insertAdjacentHTML('beforeend', makeGalleryMarkup(hits));
+  await galleryEl.insertAdjacentHTML('beforeend', makeGalleryMarkup(hits));
+
+  await gallerySLBox.refresh();
+
+  setTimeout(() => {
+    backdropLoaderEl.classList.remove('backdrop-loader-show');
+    document.body.style.overflow = 'auto';
+  }, 500);
 }
 
-function onFetchError(error) {
-  Notify.failure(error.message);
-}
-
-function addButtonLoadMore() {
-  loadMoreEl.addEventListener('click', onLoadMoreClick);
-}
-
-async function onLoadMoreClick() {
-  page += 1;
-
-  try {
-    const { data } = await fetchImages(searchName, IMAGE_PER_PAGE, page);
-
-    await onFetchSuccess(data);
-    await smoothScroll();
-    await gallerySLBox.refresh();
-
-    if (page > totalPage) {
-      await loadMoreEl.classList.remove('load-more-show');
-    }
-  } catch (error) {
-    onFetchError(error);
-  }
-}
-
-async function loadMoreWithScroll() {
+function resetToDefault() {
   loadMoreEl.classList.remove('load-more-show');
+  objectForObservation.classList.remove('scroll-guard-show');
+  backdropLoaderEl.classList.remove('backdrop-loader-show');
+  document.body.style.overflow = 'auto';
+}
 
+async function loadMoreImage() {
+  beforeSearch();
   if (page > totalPage) {
     return;
   }
   page += 1;
   try {
     const { data } = await fetchImages(searchName, IMAGE_PER_PAGE, page);
-
     await onFetchSuccess(data);
-    await gallerySLBox.refresh();
+    await smoothScroll();
   } catch (error) {
-    onFetchError(error);
+    resetToDefault(error);
   }
-}
-
-function smoothScroll() {
-  const { height: cardHeight } =
-    galleryEl.firstElementChild.getBoundingClientRect();
-
-  window.scrollBy({
-    top: cardHeight * 2,
-    behavior: 'smooth',
-  });
 }
 
 function addIntersectionObserver() {
@@ -131,12 +131,12 @@ function addIntersectionObserver() {
   const observer1 = new IntersectionObserver(entries => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
-        
-        if (infiniteScrollEl.checked) {
-          loadMoreWithScroll();
+        if (infiniteScrollEl.checked && page < totalPage) {
+          loadMoreImage();
         }
         if (!infiniteScrollEl.checked) {
           loadMoreEl.classList.add('load-more-show');
+          loadMoreEl.addEventListener('click', loadMoreImage);
         }
         if (page > totalPage) {
           loadMoreEl.classList.remove('load-more-show');
@@ -146,5 +146,15 @@ function addIntersectionObserver() {
     });
   }, options);
 
-  observer1.observe(document.querySelector('.scroll-guard'));
+  observer1.observe(document.querySelector('.object-for-observation'));
+}
+
+function smoothScroll() {
+  const { height: cardHeight } =
+    galleryEl.firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 1.9,
+    behavior: 'smooth',
+  });
 }
