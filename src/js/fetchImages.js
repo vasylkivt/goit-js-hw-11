0ceaf001500e8b3a8b919ae46e2e0227ad4a45a0
Taras <117749635+vasylkivt@users.noreply.github.com>
2023-06-02 13:57:04 +0300

    Update fetchImages.js

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -16,3 +16,29 @@ export const fetchImages = async (name, imagePerPage, page) => {
 
   return await axios.get(`/?${searchParams}`);
 };
+
+// class PixabayAPI {
+//   #BASE_URL = 'https://pixabay.com/api';
+//   #API_KEY = '36230302-a98b57dafca503e591043ee2d';
+//   #BASE_SEARCH_PARAMS = {
+//     key: this.#API_KEY,
+//     image_type: 'photo',
+//     orientation: 'horizontal',
+//     safesearch: true,
+//   };
+
+//   constructor({ perPage = 20 } = {}) {
+//     this.per_page = perPage;
+//   }
+
+//   async fetchImages() {
+//     const searchParams = new URLSearchParams(...this);
+//     console.log('searchParams:', searchParams);
+
+//     return await axios.get(`${this.#BASE_URL}/?${searchParams}`);
+//   }
+// }
+
+// const inst = new PixabayAPI({ per_page: 40 });
+
+// console.dir(inst);
